67434682324e3f6baf7b760df6c92e81a2d1f59e
lg <245249531@qq.com>
2020-12-08 18:58:07 +0800

    修改数据格式

---
+++ mkjson.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+# ------------------
+# Project: Aniku_electron
+# Tittle: mkjson.py
+# Version: python3.7
+# DateTime: 2020/12/5
+# Function:
+# Input:
+# Output:
+# ------------------
